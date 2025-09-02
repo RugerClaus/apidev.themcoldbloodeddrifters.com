0d4abe9f02030f07ef1c87e4fbfb1930bb1038c3
rugerclaus <rogerrock12games@gmail.com>
2025-09-02 15:35:45 -0600

    updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            CarouselImageSeeder::class,
+            HomeTextSeeder::class,
+            BandSeeder::class
         ]);
     }
 }
